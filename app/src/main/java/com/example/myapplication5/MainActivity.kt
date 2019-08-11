a395c9b8abb9a90ce734b087eedef392fa792d21
Liudmila Lydina <mypabota@gmail.com>
2019-08-12 00:33:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication5/MainActivity.kt
@@ -0,0 +1,152 @@
+package com.example.myapplication5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Context
+import org.json.JSONObject
+import android.os.AsyncTask
+import android.widget.*
+import java.net.URL
+import com.example.myapplication5.R.layout.activity_main
+import org.json.JSONArray
+import java.io.File
+import java.io.FileWriter
+import android.content.Intent
+import android.widget.AdapterView.OnItemClickListener
+
+class MainActivity : AppCompatActivity()  {
+    var lvMain : ListView? = null //
+    var mInfoTextView : TextView? = null  //=
+    val sUrl = "https://sample.fitnesskit-admin.ru/schedule/get_group_lessons_v2/1/"
+    var mArrayFitnessObject = emptyArray<Any>() //MutableListIterator<Any>  //emptyArray<Any>()
+    val mArrayKey = arrayOf("name","teacher","place","startTime","endTime","pay","weekDay","availability","description")
+    interface AListener {
+        fun doEvent()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            setContentView(activity_main)
+
+        lvMain = findViewById(R.id.test)
+        //mInfoTextView = findViewById(R.id.HelloWorld)
+
+        // Создаем поток для получения данных
+        val mNotify = Notify(this)
+        val ContentFts = GetUrlContentTask1()
+
+        ContentFts.addListener(mNotify)
+        ContentFts.execute(sUrl)
+
+        lvMain?.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, MainTwoActivity::class.java)
+            var mArrayRows = emptyArray<String>()
+
+            val mElement = mArrayFitnessObject[position] as HashMap<String,String>
+
+            intent.putExtra("mArrayObject",mElement)
+            //intent.putExtra("mArrayKey",mArrayKey)
+            startActivity(intent)
+        }
+
+    }
+
+    internal inner class GetUrlContentTask1 : AsyncTask<String, Int, String>() {
+        var listeners = emptyArray<Notify>()
+
+
+        //var listeners: emptyArray<Notify>() //Array<AListener>? = null
+        fun addListener(listener: Notify) {
+            listeners += listener
+        }
+        override fun doInBackground(vararg urls: String): String {
+            var arrayJson : JSONArray
+            try {
+                val url = URL(urls[0])
+                val mFaleJSONText = File(filesDir, "mFale.json")
+                val mRecordsForFile = FileWriter(mFaleJSONText)
+                mRecordsForFile.write(url.readText())
+                arrayJson = JSONArray(url.readText())
+                fitnessObject(arrayJson)
+                return url.readText()
+                //return url.readText()
+            }catch (h:NumberFormatException){
+                val url = File(filesDir, "mFale.json")
+                //return url.readText()
+                arrayJson = JSONArray(url.readText())
+                fitnessObject(arrayJson)
+                return url.readText()
+            }
+
+
+
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            try {
+                listeners!![0].doEvent()
+            } finally {
+
+            }
+
+        }
+        fun fitnessObject(arrayJson:JSONArray){
+
+            for (check in 0 until arrayJson.length()) {
+                val mHasp = HashMap<String,Any>()
+                val mDataRecord = arrayJson[check] as JSONObject
+                val mTeacher2 = HashMap<String,String>()
+                for (mIdKey in 0 until mArrayKey.size){
+                    val mKey = mArrayKey[mIdKey]
+                    val mValue= mDataRecord [mKey as String]
+                    if (mValue is String){
+                        mHasp.put(mKey,mValue.toString().trim())
+                    }
+                    if (mValue is Boolean){
+                        if(mValue==true){mHasp.put(mKey,"Да")}else{mHasp.put(mKey,"Нет")}
+                    }
+                    if(mValue is Int){
+                        mHasp.put(mKey,mValue.toString().trim())
+
+                    }
+
+                }
+                mTeacher2.put("name",((mDataRecord["teacher_v2"] as JSONObject)["name"]).toString())
+                mTeacher2.put("position",((mDataRecord["teacher_v2"] as JSONObject)["position"]).toString())
+                mTeacher2.put("imageUrl",((mDataRecord["teacher_v2"] as JSONObject)["imageUrl"]).toString())
+                mHasp.put("teacher_v2",mTeacher2)
+                mArrayFitnessObject += mHasp
+                //mArrayKey += "teacher,description,place,name,startTime,endTime,pay"
+            }
+        }
+
+    }
+
+
+    internal inner class Notify (mContext:Context): AListener {
+
+        var mContext = mContext
+
+        override fun doEvent() {
+
+            var mArrayString = emptyArray<String>()
+            for (check in  0..mArrayFitnessObject.size-1){
+                mArrayString += (mArrayFitnessObject[check] as HashMap<String,String>)["name"].toString()
+            }
+
+            var adapter = ArrayAdapter<String>(
+                mContext,
+                android.R.layout.simple_list_item_1,mArrayString
+            )
+
+            // присваиваем адаптер списку
+            lvMain?.setAdapter(adapter)
+
+        }
+
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/myapplication5/MainTwoActivity.kt
@@ -0,0 +1,83 @@
+package com.example.myapplication5
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main_two.*
+import android.content.Intent
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.ListView
+
+
+class MainTwoActivity : AppCompatActivity() {
+
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_two)
+        setSupportActionBar(toolbar)
+        var mArrayRows = emptyArray<String>()
+        var mArrayCol = emptyArray<String>()
+        val mArrayObject= intent.getSerializableExtra("mArrayObject") as HashMap<String,String>
+        var mRightCol = findViewById(R.id.dataRow) as ListView
+        //var mLeftCol = findViewById(R.id.nameRow) as ListView
+        var mCorrValue = HashMap<String,String>()
+        var mPay = ""
+        mCorrValue.put("name","Тренировка")
+        mCorrValue.put("teacher","Тренер")
+        mCorrValue.put("place","Зал")
+        //mCorrValue.put("weekDay","День недели")
+        //mCorrValue.put("startTime","Начало")
+        //mCorrValue.put("endTime","Окончание")
+        //mCorrValue.put("pay","Тренировка платная")"pay",
+        mCorrValue.put("availability","Кол-во мест")
+        //mCorrValue.put("description","Описание")
+        // -
+        // + Дни недели
+        var mDaysOfWeek = HashMap<Int,String>()
+        mDaysOfWeek.put(1,"Понедельник")
+        mDaysOfWeek.put(2,"Вторник")
+        mDaysOfWeek.put(3,"Среда")
+        mDaysOfWeek.put(4,"Четверг")
+        mDaysOfWeek.put(5,"Пятница")
+        mDaysOfWeek.put(6,"Суббота")
+        mDaysOfWeek.put(7,"Воскресенье")
+// -
+        //val mImage = findViewById<ImageView>(R.id.)
+        val entries:MutableSet<String> = (mCorrValue.keys)
+
+        val g = entries.toTypedArray()
+        mArrayRows += ""
+        mArrayCol += ""
+        for (mKey in 0 until entries.size){
+            mArrayRows += mCorrValue[g[mKey]].toString() + ": " + mArrayObject[g[mKey]].toString().trim()
+            //mArrayCol += mCorrValue[g[mKey]].toString()
+        }
+        if(mArrayObject["pay"]=="true"){
+            mPay = "(Занятие платное)"
+        }
+        mArrayRows += "Расписание: "+mDaysOfWeek[(mArrayObject["weekDay"] as String).toInt()] + " c " + mArrayObject["startTime"] + " по " + mArrayObject["endTime"]+mPay
+        mArrayRows += "Описание:" + mArrayObject["description"].toString().trim()
+
+        //mArrayCol += "Расписание"
+        //mArrayCol += "Описание"
+       // mArrayRows += "Ntcn"
+
+
+
+        var adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,mArrayRows
+        )
+        mRightCol.adapter=adapter
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
